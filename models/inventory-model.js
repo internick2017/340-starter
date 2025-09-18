0cfff4e0f37374a2f7898c02272c60fc7bcec907
Nick Granados <53047332+internick2017@users.noreply.github.com>
2025-09-18 18:50:32 -0300

    Refactor classification retrieval and navigation construction for improved error handling and performance

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -4,7 +4,16 @@ const pool = require("../database/")
  *  Get all classification data
  * ************************** */
 async function getClassifications(){
-  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+  try {
+    const query = {
+      name: 'get-classifications',
+      text: 'SELECT classification_id, classification_name FROM public.classification ORDER BY classification_name'
+    }
+    return await pool.query(query)
+  } catch (error) {
+    console.error('Error getting classifications:', error)
+    throw error
+  }
 }
 
 module.exports = {getClassifications}
--- utilities/index.js
+++ utilities/index.js
@@ -5,23 +5,24 @@ const Util = {}
  * Constructs the nav HTML unordered list
  ************************** */
 Util.getNav = async function (req, res, next) {
-  let data = await invModel.getClassifications()
-  let list = "<ul>"
-  list += '<li><a href="/" title="Home page">Home</a></li>'
-  data.rows.forEach((row) => {
-    list += "<li>"
-    list +=
-      '<a href="/inv/type/' +
-      row.classification_id +
-      '" title="See our inventory of ' +
-      row.classification_name +
-      ' vehicles">' +
-      row.classification_name +
-      "</a>"
-    list += "</li>"
-  })
-  list += "</ul>"
-  return list
+  try {
+    const data = await invModel.getClassifications()
+    
+    // Build navigation items array for better performance
+    const navItems = ['<li><a href="/" title="Home page">Home</a></li>']
+    
+    data.rows.forEach((row) => {
+      navItems.push(
+        `<li><a href="/inv/type/${row.classification_id}" title="See our inventory of ${row.classification_name} vehicles">${row.classification_name}</a></li>`
+      )
+    })
+    
+    return `<ul>${navItems.join('')}</ul>`
+  } catch (error) {
+    console.error('Error building navigation:', error)
+    // Return fallback navigation if database fails
+    return '<ul><li><a href="/" title="Home page">Home</a></li></ul>'
+  }
 }
 
 module.exports = Util
