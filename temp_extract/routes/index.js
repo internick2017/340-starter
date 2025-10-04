d2ed3f938a22411e463203f4432b9c1a9dd97653
Nick Granados <53047332+internick2017@users.noreply.github.com>
2025-10-03 22:41:57 -0300

    Remove all files related to the CSE Motors application, including server, routes, controllers, views, models, and database scripts, in preparation for a complete project overhaul.

--- temp_extract/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const baseController = require('../controllers/baseController');
-const utilities = require('../utilities');
-
-// Index route
-router.get('/', utilities.handleErrors(baseController.buildHome));
-
-module.exports = router;
